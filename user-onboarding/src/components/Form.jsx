9d3f7c382206d1b1774ae24ee767d3e2fdcac98a
John Afolabi <johnafolabi.da@gmail.com>
2019-11-14 14:27:30 +0100

    Step 2: Implement form validation, error messaging

--- user-onboarding/src/components/Form.jsx
+++ user-onboarding/src/components/Form.jsx
@@ -4,28 +4,34 @@ import * as Yup from "yup";
 import axios from "axios";
 
 function UserForm(props) {
+  console.log(props);
+
   return (
     <div className="formik-form">
       <Form>
+        <ErrorMessage name="name" render={msg => <div>{msg}</div>} />
         <label>
           Name:
           <Field type="text" name="name" />
         </label>
-
+        <br />
+        <ErrorMessage name="email" render={msg => <div>{msg}</div>} />
         <label>
           Email:
           <Field type="text" name="email" />
         </label>
-
+        <br />
+        <ErrorMessage name="password" render={msg => <div>{msg}</div>} />
         <label>
           Password:
           <Field type="password" name="password" />
         </label>
-
+        <br />
         <label>
           TOS:
           <Field type="checkbox" name="tos" />
         </label>
+        <br />
         <input type="submit" />
       </Form>
     </div>
@@ -40,7 +46,19 @@ const UserFormWithFormik = withFormik({
       password: "",
       tos: false
     };
-  }
+  },
+
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required("Please enter your name"),
+    email: Yup.string()
+      .email("Please enter a valid email address")
+      .required("Please enter a valid email address"),
+    password: Yup.string()
+      .required("Password required")
+      .min(8, "Password should be 8 characters minimum")
+      .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+    tos: Yup.boolean()
+  })
 })(UserForm);
 
 export default UserFormWithFormik;
